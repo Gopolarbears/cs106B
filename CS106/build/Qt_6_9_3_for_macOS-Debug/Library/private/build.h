3caa6b1b542489cc2d47a86b33893b6a7193d556
buxiangqi_mac <3160231312@qq.com>
2025-10-02 16:24:14 +0800

    section1

---
+++ CS106/build/Qt_6_9_3_for_macOS-Debug/Library/private/build.h
@@ -0,0 +1,9 @@
+#ifndef SPL_BUILD_H
+#define SPL_BUILD_H
+
+#define SPL_VERSION "2021.1"
+#define SPL_INSTALL_LOCATION ""
+#define SPL_BUILD_DATE "Thu Oct 2 10:33:50 2025"
+#define SPL_BUILD_USER "buxiangqi"
+
+#endif
